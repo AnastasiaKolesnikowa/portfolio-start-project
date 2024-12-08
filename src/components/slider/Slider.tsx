793790e2a6bc7e33dc379e9596189c82a2583957
AnastasiaKolesnikowa <an_kolesnikowa@mail.ru>
2024-12-08 18:18:57 +0300

    testimony

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from "styled-components";
+import {FlexWrapper} from "../FlexWrapper";
+
+export const Slider = () => {
+    return (
+        <StyledSlider>
+            <FlexWrapper>
+                <Slide>
+                    <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</Text>
+                    <Name>@ivan ivanow</Name>
+                </Slide>
+            </FlexWrapper>
+            <Pagination>
+                <span></span>
+                <span></span>
+                <span></span>
+            </Pagination>
+        </StyledSlider>
+    );
+};
+
+const StyledSlider = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 35px;
+`
+
+const Slide = styled.div`
+    max-width: 500px;
+    text-align: center;
+    
+`
+
+const Text = styled.p`
+    font-weight: 400;
+    font-size: 14px;
+    color: #fff;
+`
+
+const Name = styled.span`
+    font-weight: 600;
+    font-size: 16px;
+    letter-spacing: 0.06em;
+    text-transform: uppercase;
+    color: #fff;
+`
+
+const Pagination = styled.div`
+
+    span {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        margin: 5px;
+        background-color: #7572d5;
+        
+    }
+`
---
+++ src/layout/sections/testimony/Testimony.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {Icon} from "../../../components/icon/Icon";
+import {Slider} from "../../../components/slider/Slider";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+
+export const Testimony = () => {
+    return (
+        <StyledTestimony>
+          <SectionTitle>Testimony</SectionTitle>
+            <FlexWrapper direction={"column"} align={"center"}>
+                <Icon iconId={"testimony"}/>
+                <Slider/>
+            </FlexWrapper>
+        </StyledTestimony>
+    );
+};
+
+const StyledTestimony = styled.section`
+    background: #252527;
+    min-height: 50vh;
+`
+
